1d0e1c32ebe93b30e38a16724fca56bdb38f5630
Rarelito <151872734+Rarelito@users.noreply.github.com>
2026-04-15 15:04:43 +0200

    Création de la page mot de passe oublié.

---
+++ mdp-oublie/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style/header.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Nom site</title>
+</head>
+
+<header>
+    <div class="left">
+        <img alt="Logo"
+            class="icon"
+            src="path_to_icon"
+        >
+        <h1>Nom site</h1>
+    </div>
+    <ul class="options">
+        <li>
+            <a class="nav-item" href="account.php">
+                <i class="fas fa-user"></i>
+            </a>
+        </li>
+        <li>
+            <label class="nav-item">
+                <input id="togglenav" type="checkbox" hidden>
+                <i class="fas fa-bars"></i>
+                <nav>
+                    <ul>
+                        <li class="active">
+                            <a href="/">Accueil</a>
+                        </li>
+                        <!-- Reste des liens -->
+                    </ul>
+                </nav>
+            </label>
+        </li>
+    </ul>
+</header>
+
+<main>
+    
+    <div class="box">
+
+    </div>
+
+</main>
+
+</html>
+
