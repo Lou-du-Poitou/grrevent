3eed8cb8cf793ea5ff365f67d72ad047b60875d8
V / Lou du Poitou <101828845+Lou-du-Poitou@users.noreply.github.com>
2026-04-16 15:06:58 +0200

    Create error.exit.php

---
+++ elements/error.exit.php
@@ -0,0 +1,20 @@
+<?php
+
+function errorExit(string $message, ?string $err=null): void
+/**
+ * Affiche un message et quitte le script
+ * 
+ * @var string $message
+ * @var ?string $err=null
+ * 
+ * @return void (Affichage HTML)
+ */
+{
+    $html = <<<HTML
+    <b><p>$message</p></b>
+    <pre>$err</pre>
+HTML;
+
+    echo $html;
+    exit();
+}
