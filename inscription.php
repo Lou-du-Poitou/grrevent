e4309cc699f9dac2f9b3ba458d37e0cd5895b37c
V / Lou du Poitou <101828845+Lou-du-Poitou@users.noreply.github.com>
2026-04-16 10:22:38 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -1,37 +1,80 @@
 <?php 
+require_once './config/connection.php';
+require_once './config/regex.php';
+
+require_once './elements/inputs.php';
+
+require_once './actions/auth.actions.php';
+
 $title = 'Inscription';
 
+$erreur = null;
+
+/* REGEX qui servent la vérification des données */
+$regex = [
+    'pseudo' => PSEUDO_REGEX,
+    'email' => EMAIL_REGEX,
+    'password' => PASSWORD_REGEX,
+    'confirm' => PASSWORD_REGEX
+];
+
+$pseudo = '';
+$email = '';
+
+if (isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], 'inscription.php')) {
+    if (
+        isset($_POST['pseudo']) && !empty($_POST['pseudo']) &&
+        isset($_POST['email']) && !empty($_POST['email']) &&
+        isset($_POST['password']) && !empty($_POST['password']) &&
+        isset($_POST['confirm']) && !empty($_POST['confirm']) &&
+        count($_POST) === 4
+    ) {
+        $pseudo = $_POST['pseudo'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $confirm = $_POST['confirm'];
+
+        $ok = true;
+        foreach ($_POST as $key => $value) {
+            if (!preg_match($regex[$key], $value)) {
+                $ok = false;
+            }
+        }
+
+        if ($ok) {
+            if ($password !== $confirm) {
+                $erreur = "Les mots de passe ne correspondent pas";
+            } else {
+                // Continue
+                $db = connection();
+                $user = register($db, $pseudo, $email, $password);
+                
+                if ($user) {
+                    session_start();
+                    $_SESSION['user'] = $user;
+
+                    header('Location: _debug.php');
+                } else {
+                    $erreur = "Erreur de la base de données";
+                }
+            }
+        } else {
+            $erreur = "Bien tenté !";
+        }
+    }
+}
 
 require './elements/header.php'; 
 ?>
 <div class="auth-form">
     <h1><?= $title ?></h1>
+
     <form action="inscription.php" method="post">
-        <input type="text"
-            name="pseudo"
-            placeholder="Pseudo"
-            pattern="^[a-z][a-z0-9_]{2,34}$"
-            required
-        >
-        <input type="email"
-            name="email"
-            placeholder="E-mail"
-            required
-        >
-        <input type="password"
-            name="password"
-            pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" 
-            title="8 caractères minimum, une lettre et un chiffre"
-            placeholder="Mot de passe"
-            required
-        >
-        <input type="password"
-            name="confirm"
-            pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" 
-            title="8 caractères minimum, une lettre et un chiffre"
-            placeholder="Confirmer"
-            required
-        >
+        <?= pseudoInput('pseudo', 'Pseudo', $pseudo) ?>
+        <?= emailInput('email', 'E-mail', $email) ?>
+        <?= passwordInput('password', 'Mot de passe') ?>
+        <?= passwordInput('confirm', 'Confirmer') ?>
+
         <span class="cgu-check">
             <input type="checkbox" 
                 id="cgu-check"
@@ -44,10 +87,17 @@ require './elements/header.php';
                 <a href="conditions">conditions générales d'utilisation</a>
             </label>
         </span>
+
         <button type="submit" 
             title="Créer un compte"
         >Créer un compte</button>
+
+        <?php if ($erreur): ?>
+        <!-- Erreur lors de l'inscription -->
+        <p class="error"><?= $erreur ?></p>
+        <?php endif ?>
     </form>
+
     <p class="footer">Vous avez déjà un compte ? <a href="connexion.php">Se connecter</a></p>
 </div>
 <?php require './elements/footer.php' ?>
