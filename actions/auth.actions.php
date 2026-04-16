2ca423447dddf867fe1d5d08ed8f4574d9ebf038
V / Lou du Poitou <101828845+Lou-du-Poitou@users.noreply.github.com>
2026-04-16 12:02:58 +0200

    Update auth.actions.php

--- actions/auth.actions.php
+++ actions/auth.actions.php
@@ -2,7 +2,7 @@
 require_once './config/connection.php';
 require_once './class/User.php';
 
-function register(PDO $db, string $pseudo, string $email, string $password): User | bool
+function register(PDO $db, string $pseudo, string $email, string $password): User | null
 /**
  * Permet d'inscrire un nouvel utilisateur dans la base de données
  * 
@@ -12,10 +12,10 @@ function register(PDO $db, string $pseudo, string $email, string $password): Use
  * @var string $password
  * 
  * @return User L'utilisateur inséré si tout s'est bien passé
- * @return false En cas d'erreur
+ * @return null En cas d'erreur
  */
 {
-    $password = password_hash($password, '2y');
+    $password = password_hash($password, PASSWORD_DEFAULT);
 
     try {
         // Insertion du nouvel utilisateur
@@ -58,10 +58,50 @@ SQL;
         $state->setFetchMode(PDO::FETCH_CLASS, 'User');
         $user = $state->fetch();
     } catch (PDOException $err) {
-        return false;
+        return null;
     }
     
     return $user;
 }
 
-function duplicateEmail() {}
+function duplicateEmailPseudo(PDO $db, string $pseudo, string $email): bool | null
+/**
+ * Vérifie si le pseudo ou l'email est déjà utilisé
+ * 
+ * @var PDO $db Handle de connexion à la base de données
+ * @var string $pseudo
+ * @var string $email
+ * 
+ * @return bool
+ * true: Si pseudo/email déjà pris
+ * false: Si pseudo/email libre
+ * @return null En cas d'erreur
+ */
+{
+    $duplicate = false;
+
+    try {
+        $sql = <<<'SQL'
+        SELECT COUNT(*) AS duplicate
+        FROM User
+        WHERE userPseudo = :pseudo
+        OR userEmail = :email
+        LIMIT 1;
+SQL;
+
+        $state = $db->prepare($sql);
+
+        $state->bindParam(':pseudo', $pseudo);
+        $state->bindParam(':email', $email);
+
+        $state->execute();
+        $state->setFetchMode(PDO::FETCH_ASSOC);
+
+        $data = $state->fetch();
+        $duplicate = (bool)$data['duplicate'];
+    } catch (PDOException $err) {
+        return null;
+    }
+
+    return $duplicate;
+}
