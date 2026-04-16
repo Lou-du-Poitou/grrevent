ad2ddec7e756c74a63d265fe05f4a9ccccc2c283
V / Lou du Poitou <101828845+Lou-du-Poitou@users.noreply.github.com>
2026-04-16 10:23:25 +0200

    Update header.php

--- elements/header.php
+++ elements/header.php
@@ -2,49 +2,50 @@
 L'importer au début à chaques page:
 require './elements/header.php';
 -->
+<?php require_once './elements/nav.php' ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title><?= isset($title) ? $title : 'Nom site' ?></title>
-    <link rel="icon" href="path_to_icon">
-    <link rel="stylesheet" href="/style/index.css">
+
+    <link rel="icon" 
+        href="path_to_icon"
+    >
+
+    <link rel="stylesheet" 
+        href="/style/index.css"
+    >
     <!-- FontAwesome pour les icones -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+    <link rel="stylesheet" 
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+    >
 </head>
 <body>
     <header>
         <div class="left">
             <img alt="Logo"
                 class="icon"
-                src="path_to_icon"
+                src="https://res.cloudinary.com/dss2ops7q/image/upload/v1775053775/posts/2_1775053775052.webp"
             >
             <h1>Nom site</h1>
         </div>
+
         <ul class="options">
             <li>
                 <a class="nav-item" href="account.php">
                     <i class="fas fa-user"></i>
                 </a>
             </li>
+
             <li>
                 <label class="nav-item">
                     <input id="togglenav" type="checkbox" hidden>
                     <i class="fas fa-bars"></i>
-                    <nav>
-                        <ul>
-                            <li class="active">
-                                <a href="/">Accueil</a>
-                            </li>
-                            <li>
-                                <a href="">Autre</a>
-                            </li>
-                            <li>
-                                <a href="">Autre</a>
-                            </li>
-                        </ul>
-                    </nav>
+                    <?= navMenu() ?>
                 </label>
             </li>
         </ul>
