9ce09e7e4ce1fbae1e2f6d651d980555e41e094a
V / Lou du Poitou <101828845+Lou-du-Poitou@users.noreply.github.com>
2026-04-17 14:05:41 +0200

    Update auth.actions.php

--- actions/auth.actions.php
+++ actions/auth.actions.php
@@ -1,6 +1,10 @@
 <?php
 require_once './config/connection.php';
+
 require_once './class/User.php';
+require_once './class/Mail.php';
+require_once './class/MailTemplate.php';
+require_once './class/HostUrl.php';
 
 require_once './elements/error.exit.php';
 
@@ -35,9 +39,9 @@ SQL;
 
         $state = $db->prepare($insertSql);
 
-        $state->bindParam(':pseudo', $pseudo, PDO::PARAM_STR);
-        $state->bindParam(':email', $email, PDO::PARAM_STR);
-        $state->bindParam(':password', $password, PDO::PARAM_STR);
+        $state->bindParam(':pseudo', $pseudo);
+        $state->bindParam(':email', $email);
+        $state->bindParam(':password', $password);
 
         $state->execute();
         $state->closeCursor();
@@ -55,7 +59,7 @@ SQL;
         $userId = $db->lastInsertId();
 
         $state = $db->prepare($selectSql);
-        $state->bindParam(':id', $userId);
+        $state->bindParam(':id', $userId, PDO::PARAM_INT);
         $state->execute();
 
         $state->setFetchMode(PDO::FETCH_CLASS, 'User');
@@ -147,4 +151,126 @@ SQL;
     }
 
     return $user;
+}
+
+function requestResetPassword(PDO $db, string $email): bool
+/**
+ * Permet de faire suivre la demande de réinitialisation de mot de passe
+ * 
+ * @var PDO $db Handle de connexion à la base de données
+ * @var string $email
+ * 
+ * @return
+ */
+{
+    try {
+        $selectSql = <<<'SQL'
+        SELECT userId,
+            userPseudo
+        FROM User
+        WHERE userEmail = :email;
+SQL;
+
+        $state = $db->prepare($selectSql);
+        $state->bindParam(':email', $email);
+        $state->execute();
+
+        $data = $state->fetch(PDO::FETCH_ASSOC);
+        $state->closeCursor();
+
+        if (isset($data['userId']) && isset($data['userPseudo'])) {
+            $insertSql = <<<'SQL'
+            INSERT INTO Token (
+                userId,
+                tokenValue
+            ) VALUES (
+                :userId,
+                :tokenHash
+            ) ON DUPLICATE KEY UPDATE
+            tokenValue = :tokenHash,
+            tokenExpires = (NOW() + INTERVAL 48 HOUR);
+SQL;
+            $userId = $data['userId'];
+            $userPseudo = $data['userPseudo'];
+
+            // On génère un token qu'on envera par mail, et le hashage qu'on stockera
+            $token = bin2hex(random_bytes(32));
+            $tokenHash = password_hash($token, PASSWORD_DEFAULT);
+
+            $state = $db->prepare($insertSql);
+            $state->bindParam('userId', $userId, PDO::PARAM_INT);
+            $state->bindParam('tokenHash', $tokenHash);
+
+            $state->execute();
+
+            $link = HostUrl::path("/motpasse.php?token=$token&id=$userId"); // Lien pour réinitialiser le mot de passe
+            $template = MailTemplate::requestResetPassword($userPseudo, $link);
+
+            $mail = new Mail($email, true);
+            $mail->setSubject('Réinitialisation mot de passe');
+            $mail->setMessage($template);
+            $mail->send();
+        } else {
+            return false;
+        }
+    } catch (PDOException $err) {
+        errorExit(DB_ERROR_MESSAGE, $err);
+    }
+
+    return true;
+}
+
+function resetPassword(PDO $db, int $id, string $token, string $password)
+/**
+ * Permet de faire suivre la demande de réinitialisation de mot de passe
+ * 
+ * @var PDO $db Handle de connexion à la base de données
+ * @var int $id
+ * @var string $password
+ * 
+ * @return
+ */
+{
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    try {
+        $selectSql = <<<'SQL'
+        SELECT tokenValue
+        FROM Token
+        WHERE userId = :id
+        AND tokenExpires > NOW();
+SQL;
+
+        $state = $db->prepare($selectSql);
+        $state->bindParam(':id', $id, PDO::PARAM_INT);
+
+        $state->execute();
+
+        $data = $state->fetch(PDO::FETCH_ASSOC);
+        $state->closeCursor();
+
+        // Vérification de la validité du token
+        if (isset($data['tokenValue']) && password_verify($token, $data['tokenValue'])) {
+            $updateSql = <<<'SQL'
+            UPDATE User
+            SET userPassword = :password
+            WHERE userId = :id;
+            DELETE FROM Token
+            WHERE userId = :id;
+SQL;
+
+            $state = $db->prepare($updateSql);
+            $state->bindParam(':password', $password);
+            $state->bindParam(':id', $id);
+
+            $state->execute();
+            $state->closeCursor();
+        } else {
+            return false;
+        }
+    } catch (PDOException $err) {
+        errorExit(DB_ERROR_MESSAGE, $err);
+    }
+
+    return true;
 }
