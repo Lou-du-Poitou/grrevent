340cf1fc5983fb24e8641af1dc75eb5427f35675
V / Lou du Poitou <101828845+Lou-du-Poitou@users.noreply.github.com>
2026-04-17 14:19:52 +0200

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -1,19 +1,19 @@
 <?php
+require_once './elements/error.exit.php';
+
+define('DB_ERROR_MESSAGE', 'Erreur base de données:');
+
 function connection() {
+    $db = null;
+    
     try {
         $dsn = 'mysql:host=localhost;charset=utf8;port=3306;dbname=projetweb';
         $db = new PDO($dsn, 'root', '', [
             PDO::ATTR_PERSISTENT => true
         ]);
     } catch (PDOException $err) {
-        echo '<pre>';
-        var_dump($err);
-        echo '</pre>';
-        
-        exit();
+        errorExit(DB_ERROR_MESSAGE, $err);
     }
 
     return $db;
 }
-
-define('DB_ERROR_MESSAGE', 'Erreur base de données:');
