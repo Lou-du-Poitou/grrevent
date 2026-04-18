4e4d82d63618730e6e2678fec2c0688ea08aeb65
V / Lou du Poitou <101828845+Lou-du-Poitou@users.noreply.github.com>
2026-04-18 14:44:15 +0200

    Update header.php

--- elements/header.php
+++ elements/header.php
@@ -3,7 +3,15 @@ L'importer au début à chaques page:
 require './elements/header.php';
 -->
 <?php 
-function headerItem(string $link, string $icon): string
+require_once './config/constants.php';
+
+require_once './elements/icon.php';
+
+require_once './class/User.php';
+require_once './class/Logged.php';
+require_once './class/HostUrl.php';
+
+function headerItem(string $link, string $iconName): string
 /**
  * Item du header
  * 
@@ -13,16 +21,22 @@ function headerItem(string $link, string $icon): string
  * @return string (Composant html)
  */
 {
+    $icon = icon($iconName);
+
     $html = <<<HTML
     <li>
         <a class="header-item" href="$link">
-            <i class="fas fa-$icon"></i>
+            $icon
         </a>
     </li>
 HTML;
 
     return $html;
 }
+
+if (!isset($title)) $title = SITE_NAME;
+
+$logged = new Logged();
 
 require_once './elements/nav.php';
 ?>
@@ -33,7 +47,27 @@ require_once './elements/nav.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title><?= isset($title) ? $title : 'Nom site' ?></title>
+    <title><?= $title ?></title>
+
+    <!-- Balises meta -->
+    <?php if (isset($metaDescription)): ?>
+    <meta name="description" content="<?= $metaDescription ?>">
+    <meta property="og:description" content="<?= $metaDescription ?>">
+    <?php endif; ?>
+
+    <?php if (isset($metaImage)): ?>
+    <meta property="og:image" content="<?= $metaImage ?>">
+    <?php endif ?>
+
+    <?php if (isset($metaKeywords)): ?>
+    <meta property="og:keywords" content="<?= $metaKeywords ?>">
+    <?php endif; ?>
+    
+    <meta property="og:title" content="<?= $title ?>">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content=<?= str_replace(' ', '', SITE_NAME) ?>>
+    <meta property="og:url" content="<?= htmlspecialchars(HostUrl::path($_SERVER['REQUEST_URI'])) ?>">
+    <meta name="theme-color" content="#777777">
 
     <link rel="icon" 
         href="https://res.cloudinary.com/dss2ops7q/image/upload/v1775053775/posts/2_1775053775052.webp"
@@ -52,19 +86,32 @@ require_once './elements/nav.php';
         <div class="left">
             <img alt="Logo"
                 class="icon"
-                src="https://res.cloudinary.com/dss2ops7q/image/upload/v1775053775/posts/2_1775053775052.webp"
+                src="path_to_icon"
             >
-            <h1>Nom site</h1>
+            <h1><?= SITE_NAME ?></h1>
+        </div>
+
+        <div class="center">
+            <?php if ($logged->is()): ?>
+            <p><?= $logged->user()->getHTML('userPseudo') ?></p>
+            <?php endif ?>
         </div>
 
         <ul class="options">
+            <?php if ($logged->is()): ?>
             <?= headerItem('account.php', 'user') ?>
+            <?= headerItem('deconnexion.php', 'right-from-bracket') ?>
+
+            <?php else: ?>
+            <?= headerItem('connexion.php', 'right-to-bracket') ?>
+
+            <?php endif ?>
 
             <li>
                 <label class="header-item">
                     <input id="togglenav" type="checkbox" hidden>
                     <i class="fas fa-bars"></i>
-                    <?= navMenu() ?>
+                    <?= navMenu($logged->is()) ?>
                 </label>
             </li>
         </ul>
