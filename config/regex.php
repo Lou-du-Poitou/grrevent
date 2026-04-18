f28723c4df96df916430f18f416b5184d047b282
V / Lou du Poitou <101828845+Lou-du-Poitou@users.noreply.github.com>
2026-04-18 14:42:21 +0200

    Update and rename regex.php to constants.php

--- config/regex.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('PSEUDO_REGEX', "/^[a-z][a-z0-9_]{2,34}$/");
-define('EMAIL_REGEX', "/^[\-\.+a-zA-Z0-9_]+@[\-\.+a-zA-Z0-9_]+\.[a-zA-Z0-9_]{2,}$/");
-define('PASSWORD_REGEX', "/^(?=.*[A-Za-z])(?=.*\d).{8,}$/");
-define('TOKEN_REGEX', "/^[0-9a-f]{64}$/");
