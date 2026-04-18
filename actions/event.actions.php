1fb835aa1d0075c3ef79c343298aa130b3306493
V / Lou du Poitou <101828845+Lou-du-Poitou@users.noreply.github.com>
2026-04-18 18:40:28 +0200

    Update event.actions.php

--- actions/event.actions.php
+++ actions/event.actions.php
@@ -60,6 +60,138 @@ SQL;
     return $event;
 }
 
+function isAddedEvent(PDO $db, int $loggedId, int $eventId): bool
+/**
+ * Vérifie si un utilisateur connecté a ajouté un événement
+ * 
+ * @var PDO $db
+ * @var int $loggedId
+ * @var int $eventId
+ * 
+ * @return bool
+ */
+{
+    try {
+        $sql = <<<'SQL'
+        SELECT count(*) AS isAdded
+        FROM UserAddEvent
+        WHERE userId = :logged
+        AND eventId = :event;
+SQL;
+
+        $state = $db->prepare($sql);
+
+        $state->bindParam(':logged', $loggedId, PDO::PARAM_INT);
+        $state->bindParam(':event', $eventId, PDO::PARAM_INT);
+
+        $state->execute();
+
+        $data = $state->fetch(PDO::FETCH_ASSOC);
+
+        if ($data) {
+            $isAdded = $data['isAdded'];
+        }
+    } catch (PDOException $err) {
+        errorExit(DB_ERROR_MESSAGE, $err);
+    }
+
+    return $isAdded;
+}
+
+function addEvent(PDO $db, int $loggedId, int $eventId): true
+/**
+ * Permet à un utilisateur d'ajouter un événement
+ * 
+ * @var PDO $db
+ * @var int $loggedId
+ * @var int $eventId
+ * 
+ * @return true
+ */
+{
+    try {
+        $sql = <<<'SQL'
+        INSERT IGNORE INTO UserAddEvent (
+            userId,
+            eventId
+        ) VALUES (
+            :logged,
+            :event
+        );
+SQL;
+
+        $state = $db->prepare($sql);
+
+        $state->bindParam(':logged', $loggedId, PDO::PARAM_INT);
+        $state->bindParam(':event', $eventId, PDO::PARAM_INT);
+
+        $state->execute();
+    } catch (PDOException $err) {
+        errorExit(DB_ERROR_MESSAGE, $err);
+    }
+
+    return true;
+}
+
+function removeEvent(PDO $db, int $loggedId, int $eventId): true
+/**
+ * Permet à un utilisateur de retirer un événement
+ * 
+ * @var PDO $db
+ * @var int $loggedId
+ * @var int $eventId
+ * 
+ * @return true
+ */
+{
+    try {
+        $sql = <<<'SQL'
+        DELETE FROM UserAddEvent
+        WHERE userId = :logged
+        AND eventId = :event;
+SQL;
+
+        $state = $db->prepare($sql);
+
+        $state->bindParam(':logged', $loggedId, PDO::PARAM_INT);
+        $state->bindParam(':event', $eventId, PDO::PARAM_INT);
+
+        $state->execute();
+    } catch (PDOException $err) {
+        errorExit(DB_ERROR_MESSAGE, $err);
+    }
+
+    return true;
+}
+
+function deleteEvent(PDO $db, int $eventId): true
+/**
+ * Permet de supprimer un événement
+ * 
+ * @var PDO $db
+ * @var int $eventId
+ * 
+ * @return true
+ */
+{
+    try {
+        $sql = <<<'SQL'
+        DELETE FROM Event
+        WHERE eventId = :event;
+SQL;
+
+        $state = $db->prepare($sql);
+
+        $state->bindParam(':event', $eventId, PDO::PARAM_INT);
+
+        $state->execute();
+    } catch (PDOException $err) {
+        errorExit(DB_ERROR_MESSAGE, $err);
+    }
+
+    return true;
+}
+
 function selectUserEvents(PDO $db, int $user, int $offset) {
     try {
         $sql = <<<'SQL'
